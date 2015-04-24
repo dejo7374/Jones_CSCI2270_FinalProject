b9eb04b78dd0a3c2fe93ff619d93babfd81ace6e
dejo7374 <devon.jones@colorado.edu>
2015-04-23 18:24:35 -0600

    updated Graph defined Creature class to represnent players and enemies

---
+++ Creature.h
@@ -0,0 +1,33 @@
+#ifndef Creature
+#define Creature
+#include <string>
+#include <cstdlib> //rand function
+
+//***************************
+// Class for use as the player character
+// and enemies. Store statistics to
+// represent fighting abilities.
+// Also tracks health.
+//***************************
+
+Class Creature
+{
+    public:
+        Creature();
+        //getter methods
+        int get_hit(); //get current hitpoints
+        int get_maxhit();  //get max hitpoints
+        int get_Str(); //get Strength
+        int get_Dex(); //get Dexterity
+    protected:
+    private:
+        int hitpoints;      //current health total
+        int maxhitpoints;   //maximum health total
+        int Strength;       //Strength attribute for combat resolution
+        int Dexterity;      //Dexterity attribute for combat resolution
+};
+
+
+
+
+#endif // Creature
