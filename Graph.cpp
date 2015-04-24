4f898039cd4576bcf35b006cc883d30e8a66aa58
dejo7374 <devon.jones@colorado.edu>
2015-04-24 12:10:34 -0600

    Include Creature in Graph class

--- Graph.cpp
+++ Graph.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+Graph::Graph(){
+
+}
+
+Graph::~Graph(){
+ //destructor
+}
 
--- Graph.h
+++ Graph.h
@@ -2,6 +2,8 @@
 #define Graph
 #include <iostream>
 #include <vector>
+#include <Creature.h>
+
 //***************************
 // Graph class to create a "map" of rooms
 // in the game world. Each room is identified by
@@ -11,6 +13,8 @@
 struct Room{
     int roomNumber;
     std::string description;
+    bool enemyPresent;
+    bool visited;
     std::vector<adjRoom> adj; //
 };
 
@@ -24,11 +28,13 @@ class Graph()
     public:
         Graph();
         ~Graph();
-        void addVertex(int roomNumber);
-        void addEdge(std::string vl, std::string v2, int weight);
-        void displayEdges();
+        void addRoom(int roomNumber);
+        Room* createRoom(int roomNumber);
+        void addEdge(int room1, int room2);
+        void placeEnemies();
+        void
     protected:
     private:
-        std::vector<vertex> vertices;
+        std::vector<Room> rooms;
 };
 #endif // GRAPH
